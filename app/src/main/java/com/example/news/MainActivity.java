f4b307b93bbb162af08f4f053ade42e3d896bf28
Roy <devedroy.dr@gmail.com>
2022-08-03 14:47:01 +0530

    init

--- app/src/main/java/com/example/news/MainActivity.java
+++ app/src/main/java/com/example/news/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    /*
+    * project initialization
+    * */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
